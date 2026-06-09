74a95f9041b08d631bdc52c7fff22885e44fc778
Marco Catalan <mcat95@gmail.com>
2026-06-09 11:42:56 +0200

    Simplify build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
+set -e
 
-rm -rf $PWD/dist
+rm -rf "$PWD/dist"
 babel src --out-dir dist --ignore '**/*-unit.js' --source-maps inline
-git reset
-git add $PWD/dist
-git commit -m 'Updating dist files' -n
